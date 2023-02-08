ed1684c0f18e1f16cca418b7707b85b510bca579
Winifred <winifredasante15@gmail.com>
2023-02-08 00:30:45 -0500

    commit

--- src/components/PopularSection.js
+++ src/components/PopularSection.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./popularsection.css";
-import './ParentStyle.css'
+import "./ParentStyle.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import CloseBtn from "../images/close1.png";
@@ -9,7 +9,6 @@ import CloseBtn from "../images/close1.png";
 import { Navigation } from "swiper";
 
 const PopularSection = () => {
-
   const toggleShowRoom = () => {
     const modalScreen = document.querySelectorAll(".modal-screen");
     modalScreen.forEach((screen) => {
@@ -27,16 +26,15 @@ const PopularSection = () => {
 
   const responsiveOptions = {
     320: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
+      slidesPerView: 3,
+    },
   };
-  
 
   return (
     <>
@@ -47,9 +45,9 @@ const PopularSection = () => {
 
           <Swiper
             navigation={{
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev', 
-              clickable: true
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+              clickable: true,
             }}
             modules={[Navigation]}
             spaceBetween={32}
@@ -61,26 +59,26 @@ const PopularSection = () => {
             className="mySwiper popular_container"
           >
             <SwiperSlide>
-              <article class="popular_card swiper-slide">
-                {/* modal */}
-                <div className="modal-screen">
-                  <div className="modal">
-                    <div className="popular_data">
-                      <h3 className="popular_title">The red Beach</h3>
-                      <p className="popular_description">
-                        Another one
-                        <br />
-                        Tokyo
-                        <br />
-                        P.O.Box 10
-                      </p>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Blue Beach</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
 
-                      <button className="popular_button">Book</button>
-                    </div>
-                    <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                    <button className="popular_button">Book</button>
                   </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
                 </div>
-                {/* modal */}
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
                 <img src="./images/beach.jpg" class="popular_img" alt="House" />
 
                 <div class="popular_data">
@@ -102,6 +100,25 @@ const PopularSection = () => {
               </article>
             </SwiperSlide>
             <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">Golden House</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
               <article class="popular_card swiper-slide">
                 <img
                   src="./images/america.jpg"
@@ -122,9 +139,31 @@ const PopularSection = () => {
                     P.O.Box 190
                   </p>
                 </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
               </article>
             </SwiperSlide>
             <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Blue Sky</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
               <article class="popular_card swiper-slide">
                 <img
                   src="./images/IMG-20220422-WA0009.jpg"
@@ -145,32 +184,368 @@ const PopularSection = () => {
                     P.O.Box 190
                   </p>
                 </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
               </article>
             </SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Green Garden</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+                <img
+                  src="./images/IMG-20220422-WA0024.jpg"
+                  class="popular_img"
+                  alt="House"
+                />
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>69,000
+                  </h2>
+                  <h3 class="popular_title">The Green Garden</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    East Legon street,Abelenkpe-Accra
+                    <br />
+                    P.O.Box 190
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Mysterious House</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+                <img
+                  src="./images/pexels-pixabay-531234.jpg"
+                  class="popular_img"
+                  alt="House"
+                />
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>6,000
+                  </h2>
+                  <h3 class="popular_title">The Mysterious House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    Hamburg, Germany
+                    <br />
+                    P.O.Box 110
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <div class="swiper-button-next">
-            <i class="bx bx-chevron-right"></i>
-          </div>
-          <div class="swiper-button-prev">
-            <i class="bx bx-chevron-left"></i>
-          </div>
+              <i class="bx bx-chevron-right"></i>
+            </div>
+            <div class="swiper-button-prev">
+              <i class="bx bx-chevron-left"></i>
+            </div>
           </Swiper>
         </div>
-        
+      </section>
+
+      <section class="popular section" id="explore">
+        <div class="container">
+          <span class="section_subtitle">Travel</span>
+          <h2 class="section_title">Popular Places</h2>
+
+          <Swiper
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+              clickable: true,
+            }}
+            modules={[Navigation]}
+            spaceBetween={32}
+            grabCursor={true}
+            centeredSlides={true}
+            breakpoints={responsiveOptions}
+            slidesPerView={3}
+            loop={true}
+            className="mySwiper popular_container"
+          >
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Blue Beach</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+                <img
+                  src="./images/tokyo (2).jpg"
+                  class="popular_img"
+                  alt="House"
+                />
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>66,000
+                  </h2>
+                  <h3 class="popular_title">Red House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    Tokyo
+                    <br />
+                    P.O.Box 10
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">Golden House</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+              <img src="./images/brooklyn.jpg" class="popular_img"
+                                alt="House"/>
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>56,500
+                  </h2>
+                  <h3 class="popular_title">Golden House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    Brooklyn
+                    <br />
+                    P.O.Box 190
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Blue Sky</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+              <img src="./images/chicago.jpg" class="popular_img"
+                                alt="House"/>
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>46,080
+                  </h2>
+                  <h3 class="popular_title">Round House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    Chicago
+                    <br />
+                    P.O.Box 190
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Green Garden</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+              <img src="./images/nick-fewings-jBmNAt2p8-s-unsplash.jpg" class="popular_img" alt="House"/>
+
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>69,000
+                  </h2>
+                  <h3 class="popular_title">Blue House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    East Legon street,Abelenkpe-Accra
+                    <br />
+                    P.O.Box 190
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <SwiperSlide>
+              {/* modal */}
+              <div className="modal-screen">
+                <div className="modal">
+                  <div className="popular_data">
+                    <h3 className="popular_title">The Mysterious House</h3>
+                    <p className="popular_description">
+                      Another one
+                      <br />
+                      Tokyo
+                      <br />
+                      P.O.Box 10
+                    </p>
+
+                    <button className="popular_button">Book</button>
+                  </div>
+                  <img src={CloseBtn} alt="btn" onClick={closeShowRoom} />
+                </div>
+              </div>
+              {/* modal */}
+              <article class="popular_card swiper-slide">
+              <img src="./images/austin-distel-wawEfYdpkag-unsplash.jpg" class="popular_img" alt="House"/>
+
+                <div class="popular_data">
+                  <h2 class="popular_price">
+                    <span>$</span>66,000
+                  </h2>
+                  <h3 class="popular_title">Noble House</h3>
+                  <p class="popular_description">
+                    wI_tRAVeL Agency,
+                    <br />
+                    Hamburg, Germany
+                    <br />
+                    P.O.Box 110
+                  </p>
+                </div>
+                <button className="popular_button" onClick={toggleShowRoom}>
+                  Visit
+                </button>
+              </article>
+            </SwiperSlide>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div class="swiper-button-next">
+              <i class="bx bx-chevron-right"></i>
+            </div>
+            <div class="swiper-button-prev">
+              <i class="bx bx-chevron-left"></i>
+            </div>
+          </Swiper>
+        </div>
       </section>
     </>
   );
